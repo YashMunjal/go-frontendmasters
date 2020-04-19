7d0cb4c2a2399b99806c6b3b06642a95a047b261
YashMunjal <yashmu45@gmail.com>
2020-04-20 04:15:11 +0530

    module two completed

--- complex_structures_code/complex_structures.go
+++ complex_structures_code/complex_structures.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import ( "fmt"
+)
+
+//part 1
 func average(scores [5]float64) float64 {
 	total:=0.0
 	for _,num :=range scores {
@@ -9,8 +12,27 @@ func average(scores [5]float64) float64 {
 	return total/float64(len(scores))
 }
 
+//part 2
+
+var initialPets map[string]string= map[string]string{
+	"saksham":"mosquito",
+	"bansal":"panda",
+	"deepankur":"scorpion",
+	"vaibhavMittal":"doggo",
+
+}
+func doesPetExists(petName string) bool{
+	_,ok :=initialPets[petName]
+	return ok
+}
 func main(){
 	scores := [5]float64{2,7,8,9}
-	fmt.Println(scores)
-	fmt.Println(float64(average))
+	//fmt.Println(scores)
+	fmt.Println(average(scores))
+
+	fmt.Println(doesPetExists("bansal"))
+	//returns true
 }
+
+
+
